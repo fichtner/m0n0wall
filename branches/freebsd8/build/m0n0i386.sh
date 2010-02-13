82cc902e315097ae341a0878992534e1cb930691
awhite <awhite@e36fee2c-cc09-0410-a7cc-ebac5c6737de>
2010-02-13 09:54:25 +0000

    First draft of instructions to build under version 8 of freebsd
    git-svn-id: http://svn.m0n0.ch/wall@362 e36fee2c-cc09-0410-a7cc-ebac5c6737de

---
+++ branches/freebsd8/build/m0n0i386.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# $FreeBSD: src/tools/tools/nanobsd/rescue/isoi386.sh,v 1.1.2.1.2.1 2009/10/25 01:10:29 kensmith Exp $
+#
+
+sh /usr/src/tools/tools/nanobsd/nanobsd.sh -i $* -c m0n0i386.conf -b 
