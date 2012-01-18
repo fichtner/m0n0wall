f0f68ccdf3cbb4610970c7472e0555531094fb4d
mkasper <mkasper@e36fee2c-cc09-0410-a7cc-ebac5c6737de>
2012-01-18 11:00:53 +0000

    - fix embedded platform display
    - port dummynet+ipnat double processing fix from 6.4
    
    git-svn-id: http://svn.m0n0.ch/wall@483 e36fee2c-cc09-0410-a7cc-ebac5c6737de

--- branches/freebsd8/build/scripts/4buildkernel.sh
+++ branches/freebsd8/build/scripts/4buildkernel.sh
@@ -32,6 +32,7 @@ fi
 		patch < $MW_BUILDPATH/freebsd8/build/patches/kernel/dummynet_v6.patch
 		patch < $MW_BUILDPATH/freebsd8/build/patches/kernel/r222728_defroute.patch
 		patch < $MW_BUILDPATH/freebsd8/build/patches/kernel/ip_fil_freebsd.c.patch
+		patch < $MW_BUILDPATH/freebsd8/build/patches/kernel/dummynet_with_ipnat.patch
 		
 # kernel compile
         cd $MW_BUILDPATH/tmp/sys/$MW_ARCH/conf
