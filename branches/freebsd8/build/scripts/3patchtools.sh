70096454e0bd067e380709ba9d6fedb850500984
awhite <awhite@e36fee2c-cc09-0410-a7cc-ebac5c6737de>
2013-07-23 23:39:44 +0000

    Silence RA announce receive log message for WAN interfaces when using dhcp-pd
    git-svn-id: http://svn.m0n0.ch/wall@543 e36fee2c-cc09-0410-a7cc-ebac5c6737de

--- branches/freebsd8/build/scripts/3patchtools.sh
+++ branches/freebsd8/build/scripts/3patchtools.sh
@@ -8,37 +8,44 @@ if [ -z "$MW_BUILDPATH" -o ! -d "$MW_BUILDPATH" ]; then
 fi
 
 # syslogd circular logging support and ipv6 support
-		rm -Rf /usr/obj/usr/src/usr.sbin/syslogd
-		rm -Rf /usr/obj/usr/src/usr.sbin/clog
-		cd /usr/src/usr.sbin
-		tar xfvz $MW_BUILDPATH/freebsd8/build/patches/user/clog-1.0.1.tar.gz
-        cd syslogd
-        patch < $MW_BUILDPATH/freebsd8/build/patches/user/syslogd.c.patch
-        make obj && make
-        install -s /usr/obj/usr/src/usr.sbin/syslogd/syslogd $MW_BUILDPATH/m0n0fs/usr/sbin/
-		mv syslogd.c.orig syslogd.c
-		cd ../clog
-        make obj && make
-        install -s /usr/obj/usr/src/usr.sbin/clog/clog $MW_BUILDPATH/m0n0fs/usr/sbin/
-		cd ..
-		rm -Rf clog
+	rm -Rf /usr/obj/usr/src/usr.sbin/syslogd
+	rm -Rf /usr/obj/usr/src/usr.sbin/clog
+	cd /usr/src/usr.sbin
+	tar xfvz $MW_BUILDPATH/freebsd8/build/patches/user/clog-1.0.1.tar.gz
+	cd syslogd
+	patch < $MW_BUILDPATH/freebsd8/build/patches/user/syslogd.c.patch
+	make obj && make
+	install -s /usr/obj/usr/src/usr.sbin/syslogd/syslogd $MW_BUILDPATH/m0n0fs/usr/sbin/
+	mv syslogd.c.orig syslogd.c
+	cd ../clog
+	make obj && make
+	install -s /usr/obj/usr/src/usr.sbin/clog/clog $MW_BUILDPATH/m0n0fs/usr/sbin/
+	cd ..
+	rm -Rf clog
 # dhclient-script
-        cp $MW_BUILDPATH/freebsd8/build/tools/dhclient-script $MW_BUILDPATH/m0n0fs/sbin
-        chmod a+rx $MW_BUILDPATH/m0n0fs/sbin/dhclient-script
+	cp $MW_BUILDPATH/freebsd8/build/tools/dhclient-script $MW_BUILDPATH/m0n0fs/sbin
+	chmod a+rx $MW_BUILDPATH/m0n0fs/sbin/dhclient-script
 # ifconfig for r222728
-		rm -Rf /usr/obj/usr/src/sbin/ifconfig
-		rm -Rf $MW_BUILDPATH/tmp/netinet6
-		cp -r /usr/src/sys/netinet6 $MW_BUILDPATH/tmp
-		cd $MW_BUILDPATH/tmp/netinet6
-		patch -p2 -t < $MW_BUILDPATH/freebsd8/build/patches/kernel/r222728_defroute.patch
-		cd /usr/src/sbin/ifconfig
-		patch < $MW_BUILDPATH/freebsd8/build/patches/user/ifconfig.r222728.patch
-		patch < $MW_BUILDPATH/freebsd8/build/patches/user/ifconfig.Makefile.patch
-		make obj && make
-		install -s /usr/obj/usr/src/sbin/ifconfig/ifconfig $MW_BUILDPATH/m0n0fs/sbin/
-		mv Makefile.orig Makefile
-		mv af_inet6.c.orig af_inet6.c
-		mv af_nd6.c.orig af_nd6.c
+	rm -Rf /usr/obj/usr/src/sbin/ifconfig
+	rm -Rf $MW_BUILDPATH/tmp/netinet6
+	cp -r /usr/src/sys/netinet6 $MW_BUILDPATH/tmp
+	cd $MW_BUILDPATH/tmp/netinet6
+	patch -p2 -t < $MW_BUILDPATH/freebsd8/build/patches/kernel/r222728_defroute.patch
+	cd /usr/src/sbin/ifconfig
+	patch < $MW_BUILDPATH/freebsd8/build/patches/user/ifconfig.r222728.patch
+	patch < $MW_BUILDPATH/freebsd8/build/patches/user/ifconfig.Makefile.patch
+	make obj && make
+	install -s /usr/obj/usr/src/sbin/ifconfig/ifconfig $MW_BUILDPATH/m0n0fs/sbin/
+	mv Makefile.orig Makefile
+	mv af_inet6.c.orig af_inet6.c
+	mv af_nd6.c.orig af_nd6.c
+# rtadvd remove logging for dhcp-pd
+	rm -Rf /usr/obj/usr/src/usr.sbin/rtadvd
+	cd /usr/src/usr.sbin/rtadvd
+	patch < $MW_BUILDPATH/freebsd8/build/patches/user/rtadvd.dhcppd.patch
+	make obj && make
+	install -s /usr/obj/usr/src/usr.sbin/rtadvd/rtadvd $MW_BUILDPATH/m0n0fs/usr/sbin/
+	mv rtadvd.c.orig rtadvd.c
 # lets strip out any missed symbols lazy way , lots of harmless errors to dev null
 	set +e
 	find $MW_BUILDPATH/m0n0fs/ | xargs strip -s 2> /dev/null
